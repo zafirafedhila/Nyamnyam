a3c2c67068521fd924ce955877894c7e191e0bab
Zafira Fedhila <zafirafedhila@gmail.com>
2021-04-12 14:54:05 +0700

    Initial Commit

---
+++ Nyamnyam/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Nyamnyam
+//
+//  Created by Zafira Fedhila on 12/04/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
